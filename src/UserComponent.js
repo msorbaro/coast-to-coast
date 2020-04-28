438d7ae613f6ebdfe808be21b92020d02a2e0f78
brianwang26 <brian.c.wang.23@dartmouth.edu>
2020-04-28 01:09:22 -0700

    Labels Working

--- src/UserComponent.js
+++ src/UserComponent.js
@@ -1,5 +1,5 @@
 import React, { Component, PureComponent } from 'react';
-import {PieChart, Pie, Sector, Cell} from 'recharts';
+import {PieChart, Pie, Sector, Cell, Tooltip} from 'recharts';
 import fire from './config/Fire';
 
 class UserComponent extends Component{
@@ -50,11 +50,19 @@ class UserComponent extends Component{
           ];
 
           const data01 = [
-            { name: 'Group A', value: 700},
-            { name: 'Group B', value: 100 },
-            { name: 'Group C', value: 350 },
+            { name: 'Prof. Chakrabarty', value: 1000},
+            { name: 'Prof. Chakrabarti', value: 400 },
+            { name: 'Prof. Pierson', value: 600 },
+            { name: 'Prof. Cormen', value: 750}, 
+            { name: 'Prof. Jayanti', value: 900}
           ];
-          
+
+          const data02 = [
+            { time: '20F', value: 1000},
+            { time: '21W', value: 500 },
+            { time: 'After 21W', value: 200 }
+          ];
+
         const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', "#8884d8"]; 
         // Many Possible Colors for our chart-- chart.js will only use the first n we have, where n = number of answer choices we have 
 
@@ -77,38 +85,26 @@ class UserComponent extends Component{
             <div>
                 {displayedScreen}
                 <div style={{display: 'flex', justifyContent:'center', flexWrap: 'wrap', height: '100vh'}}>
+
+                    <h1> Question 1: Who is the best professor at Dartmouth? </h1>
+                    <PieChart width={500} height={500}>
+                   <Pie dataKey="value" isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={200} label = {renderCustomizedLabel}>
+                    {data01.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)}
+                    </Pie>
+                    <Tooltip />
+                    </PieChart>
+
+                    <h1> Question 2: When do you think we will return to campus? </h1>
+                    <PieChart width={500} height={500}>
+                   <Pie dataKey="value" isAnimationActive={false} data={data02} cx={200} cy={200} outerRadius={200} label = {renderCustomizedLabel}>
+                    {data02.map((entry, index) => <Cell key={`cell-${entry.time}`} fill={COLORS[index % COLORS.length]} />)}
+                    </Pie>
+                    <Tooltip />
+                    </PieChart>
                     
-                    <h1> Question 1: How are you doing today? </h1>
+                    <h1> We aren't using this chart anymore </h1>
                     <PieChart width={500} height={500}>
                     <Pie data={data} cx={200} cy={200} labelLine={false} label={renderCustomizedLabel}  outerRadius={200} fill="#8884d8" dataKey="value">
-                    {data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)}
-                    </Pie>
-                    </PieChart>
-
-                    <h1> Question 2: Why? </h1>
-                    <PieChart width={500} height={500}>
-                    <Pie data={data01} cx={200} cy={200} labelLine={false} label={renderCustomizedLabel} outerRadius={200} fill="#8884d8" dataKey="value">
-                    {data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)}
-                    </Pie>
-                    </PieChart>
-
-                    <h1> Question 3: When's the latest you think we will return to campus? </h1>
-                    <PieChart width={500} height={500}>
-                    <Pie data={data01} cx={200} cy={200} labelLine={false} label={renderCustomizedLabel} outerRadius={200} fill="#8884d8" dataKey="value">
-                    {data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)}
-                    </Pie>
-                    </PieChart>
-
-                    <h1> Question 4: When's the earliest you think we will return to campus? </h1>
-                    <PieChart width={500} height={500}>
-                    <Pie data={data01} cx={200} cy={200} labelLine={false} label={renderCustomizedLabel} outerRadius={200} fill="#8884d8" dataKey="value">
-                    {data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)}
-                    </Pie>
-                    </PieChart>
-
-                    <h1> Question 5: Who is the best professor at Dartmouth? </h1>
-                    <PieChart width={500} height={500}>
-                    <Pie data={data} cx={200} cy={200} labelLine={false} label={renderCustomizedLabel} outerRadius={200} fill="#8884d8" dataKey="value">
                     {data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)}
                     </Pie>
                     </PieChart>
